d7934e4b553449516bc38213904c0fab20b56923
Khushwant-Kumar-Suthar <khushwantsuthar49@gmail.com>
2026-05-01 15:06:10 +0530

    Add deleteByIdAndUser method to RoadmapStepRepository

---
+++ path/to/RoadmapStepRepository.java
@@ -0,0 +1,5 @@
+
+    // New method to delete a resource by its ID and associated user
+    public void deleteByIdAndUser(Long id, User user) {
+        // Your logic to delete the resource by ID and associated user goes here
+    }

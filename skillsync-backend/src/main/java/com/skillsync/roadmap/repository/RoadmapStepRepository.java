a4ae131808f4a28ff623bcf46c59e74d50e4aa3c
Khushwant-Kumar-Suthar <khushwantsuthar49@gmail.com>
2026-05-01 15:08:48 +0530

    fixing error

--- skillsync-backend/src/main/java/com/skillsync/roadmap/repository/RoadmapStepRepository.java
+++
@@ -1,4 +0,0 @@
-@Modifying
-@Transactional
-@Query("DELETE FROM RoadmapStep r WHERE r.id = :stepId AND r.user = :user")
-void deleteByIdAndUser(@Param("stepId") Long stepId, @Param("user") User user);
